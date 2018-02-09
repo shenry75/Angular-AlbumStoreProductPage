f0ac20b1fb8ac3b5e86b3016fc76ba78d3a195c5
shenry75 <shawn.henry@gmail.com>
2018-02-08 17:05:19 -0700

    Album and Track Iface
    added interfaces for album and track

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
+
+
 @Injectable()
 export class ProductService {
 
@@ -9,9 +13,9 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  public getAlbum( id: number) {
+  public getAlbum ( id: number): Observable<Album> {
         return this._http.get(this._albumUrl).map((response) =>
-        response.json());
+        <Album>response.json());
   }
 
 }
